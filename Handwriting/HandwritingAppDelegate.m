9602643608a8eead269670db504cca9f95ea2e55
GauravDS <gaurav.sharma@punchh.com>
2016-05-12 20:51:50 +0530

    run for ios 9

--- Handwriting/HandwritingAppDelegate.m
+++ Handwriting/HandwritingAppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
 	[self.window addSubview:self.controller.view];
 	[self.window makeKeyAndVisible];
     return YES;
